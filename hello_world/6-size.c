a022a5889fecd2a37c3e85c8f45d3186bbbeadda
lowbacc <skalski.matthis@gmail.com>
2024-06-18 01:13:55 +0200

    sizeholberton

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of long int: %lu byte(s)\n", sizeof(long));
 	printf("Size of long long int: %lu byte(s)\n", sizeof(long long));
