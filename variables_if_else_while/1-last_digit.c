8b2165e0fb65bf8196664818e4f11ce48c5ace75
lowbacc <skalski.matthis@gmail.com>
2024-06-18 23:45:44 +0200

    i erased s on return, this r block me

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,9 +4,10 @@
 
 /**
  * main - Entry point
- * Description: TheLastDigit
- * Returns: Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
@@ -20,16 +21,16 @@ int main(void)
 	if (LastDig > 5)
 	{
 		printf("last digit of %d is %d and is greater than 5\n", n, LastDig);
-	};
+	}
 
 	if (LastDig == 0)
 	{
 		printf("last digit of %d is %d and is 0\n", n, LastDig);
-	};
+	}
 
 	if (LastDig < 6 && LastDig != 0)
 	{
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, LastDig);
-	};
+	}
 	return (0);
 }
