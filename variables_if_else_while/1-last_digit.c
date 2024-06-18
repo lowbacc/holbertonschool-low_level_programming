8415f8ee1c5e4e059758259b4c72125cebd45454
lowbacc <skalski.matthis@gmail.com>
2024-06-18 23:55:53 +0200

    I try the command and she work

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,16 +21,19 @@ int main(void)
 	if (LastDig > 5)
 	{
 		printf("last digit of %d is %d and is greater than 5\n", n, LastDig);
-	}
+	};
+
 
 	if (LastDig == 0)
 	{
 		printf("last digit of %d is %d and is 0\n", n, LastDig);
-	}
+	};
+
 
 	if (LastDig < 6 && LastDig != 0)
 	{
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, LastDig);
-	}
+	};
+
 	return (0);
 }
