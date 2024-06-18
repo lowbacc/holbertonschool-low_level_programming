fe79fd15e3b11427facfdd7b740ad86e817afb88
lowbacc <skalski.matthis@gmail.com>
2024-06-19 01:08:35 +0200

    numbs is easy, repeat tha alphabet with a numbs

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbs = 0;
+
+	while (numbs <= 9)
+	{
+		putchar('0' + numbs);
+	};
+
+	putchar('\n');
+	return (0);
+}
