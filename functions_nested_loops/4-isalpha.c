10c8cf0b4c5b34e921f123245db67cbedc85f30d
lowbacc <skalski.matthis@gmail.com>
2024-06-22 20:56:46 +0200

    Always no problem ;)

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *@c: is an input
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	return (0);
+}
