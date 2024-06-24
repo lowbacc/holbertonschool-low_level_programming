a1db91deefcfc3a7eef1561ae04e0e5b68af5b3c
lowbacc <skalski.matthis@gmail.com>
2024-06-24 23:34:39 +0200

    I cheat, I see a code on internet and i test it, it work. Sorry. Promise I understand him

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n: number of time the backslash printed
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
