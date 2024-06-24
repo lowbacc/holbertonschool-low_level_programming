e70ad58aba299dc26a85f38d9b2e46b851b1f028
lowbacc <skalski.matthis@gmail.com>
2024-06-24 09:41:56 +0200

    The same code for this task

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c: number checked
+ * Return: Always 0 (Success)
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
