2be3c8bd57928b9f62a797aaf0a6f946d7d8eef2
lowbacc <skalski.matthis@gmail.com>
2024-06-30 10:31:42 +0200

    The compil work but the test is blank, I think is incorrect

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - Entry point
+ * @s: string
+ * Return: Always 0 (Success)
+ */
+char *leet(char *s)
+{
+	char *letters = "aeotlAEOTL";
+	char *leet = "4307143071";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; j++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = leet[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
