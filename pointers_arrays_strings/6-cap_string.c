e8cd843c161c6fdab6b2728740483b0e1b857c41
lowbacc <skalski.matthis@gmail.com>
2024-06-30 19:41:57 +0200

    I try a lot of things, I went back to my basic code beacause it is more understandable even if I don't have the point for betty. I'm taking a break again maybe I'll find a solution later by coming back to it

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,9 +2,7 @@
 
 /**
  * cap_string - Entry point
- *
  * @s: character
- *
  * Return: Always 0 (Success)
  */
 char *cap_string(char *s)
@@ -16,7 +14,6 @@ char *cap_string(char *s)
 	{
 		s[0] -= 32;
 	}
-
 	for (i = 0, j = 0; s[i] != '\0'; i++, j++)
 	{
 		if (s[i] == 9)
@@ -29,7 +26,6 @@ char *cap_string(char *s)
 		}
 	}
 	s[j] = '\0';
-
 	for (i =  1; s[i] != '\0' ; i++)
 	{
 		if (s[i] >= 97 && s[i] <= 122)
