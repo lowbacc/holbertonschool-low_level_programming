d5ad81168bc053494ca6c0c748ade914efd4bd11
lowbacc <skalski.matthis@gmail.com>
2024-07-03 15:54:46 +0200

    Almost the same code, not the same but the same principe for an other result

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arg
+ * @argv: array of arg
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
