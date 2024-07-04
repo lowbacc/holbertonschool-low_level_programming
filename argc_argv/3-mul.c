c622bef8e02fb25eec245667d37159254238f37b
lowbacc <skalski.matthis@gmail.com>
2024-07-04 14:22:39 +0200

    I rechange the code to the first I commit

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,19 +9,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-
 	if (argc != 3)
 	{
 		printf("error\n");
 		return (1);
 	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	printf("argc: %d, num1: %d, num2:%d\n", argc, num1, num2);
-
-	printf("%d\n", num1 * num2);
+	printf("%d\n", atoi(argv[1] * atoi(argv[2]);
 	return (0);
 }
