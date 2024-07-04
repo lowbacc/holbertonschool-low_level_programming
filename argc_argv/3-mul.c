0cf044b43f14a03bd16280770863984f2514b871
lowbacc <skalski.matthis@gmail.com>
2024-07-04 11:18:00 +0200

    Multiplication is a good code, I like make it

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arg
+ * @argv: array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
