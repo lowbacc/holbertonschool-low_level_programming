1960581133fdec3281fb52c9ced6c9c2708214db
lowbacc <skalski.matthis@gmail.com>
2024-07-04 11:27:42 +0200

    The checker don't like me, I add a debug line

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -20,6 +20,8 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 
+	printf("argc: %d, num1: %d, num2:%d\n", argc, num1, num2);
+
 	printf("%d\n", num1 * num2);
 	return (0);
 }
