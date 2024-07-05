bb948cc404fd09349b5dd070f0525f33ea1a78cc
lowbacc <skalski.matthis@gmail.com>
2024-07-05 13:01:54 +0200

    With number is more easier even if the main.c file make all the work

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ * @n: number
+ * Return: Always 0.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1); 
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
