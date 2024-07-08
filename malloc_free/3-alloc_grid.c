9a5edf969235a7122a46f6c7b1bd332fe32ac820
lowbacc <skalski.matthis@gmail.com>
2024-07-08 10:43:49 +0200

    The first free git add 3-alloc_grid.c wouhou

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Entry point
+ * @width: the width of the array
+ * @height: the height of the array
+ * Return: Always 0.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = malloc(height * sizeof(int *));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
+		{
+			while (i >= 0)
+				free(array[--i]);
+			free(array);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+	}
+	return (array);
+}
