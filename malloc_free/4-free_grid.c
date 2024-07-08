c4fcb792654983ff4a56429184bb2b1478100d77
lowbacc <skalski.matthis@gmail.com>
2024-07-08 11:22:45 +0200

    I'm not sure for this

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_greed - Entry point
+ * @grid: array
+ * @height: the heught of grid
+ * Return: Always 0.
+ */
+void free_grid(int **grid, int height)
+{
+	while (height--)
+		free(grid[height]);
+
+	free(grid);
+}
