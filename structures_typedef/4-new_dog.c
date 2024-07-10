e71b4a386d5d82591cf03f20cea152ac640f348f
lowbacc <skalski.matthis@gmail.com>
2024-07-10 10:20:51 +0200

    Betty problem

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,7 +11,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int i, name_len, owner_len;;
+	int i, name_len, owner_len;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
