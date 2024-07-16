ea626735d678d62fbeb4273df995543a75488b0d
lowbacc <skalski.matthis@gmail.com>
2024-07-16 14:56:19 +0200

    Try this

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Entry point
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer
+ * Return: Always 0.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i] != 0))
+				return (i);
+		}
+	}
+	return (-1);
+}
