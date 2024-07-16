828e4f0788fce0ed09de470ee3e296d2a9a990a2
lowbacc <skalski.matthis@gmail.com>
2024-07-16 20:27:08 +0200

    I don't make a while loop, now it's ok

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -18,13 +18,11 @@ int (*get_op_func(char *s))(int a, int b)
 	};
 	int i = 0;
 
-	if (!s || !*s || s[1] != '\0')
-		return (NULL);
-
-	for (i = 0; ops[i].op != NULL; i++)
+	while (ops[i].op)
 	{
-		if (s[0] == ops[i].op[0])
+		if (*s == *ops[i].op && s[1] == '\0')
 			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
