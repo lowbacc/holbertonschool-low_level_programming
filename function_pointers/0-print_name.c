6c1ca1360fc8dc28fae49545f95f5b702fec0140
lowbacc <skalski.matthis@gmail.com>
2024-07-16 10:31:02 +0200

    test

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - Entry point
+ * @name: name
+ * @f: function
+ * Return: Always 0.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
