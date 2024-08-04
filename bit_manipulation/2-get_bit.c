f6a9fc7b750ca50f5450d02fa6859d0df28856ed
lowbacc <skalski.matthis@gmail.com>
2024-08-04 08:59:02 +0200

    The task 2, not serious

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - Get the value of a bit at a given index
+ * @n: Number
+ * @index: Index
+ * Return: Value of the bit in index or -1 if an error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+    if (index >= (sizeof(n) * 8))
+        return (-1);
+
+    return ((n >> index) & 1);
+}
